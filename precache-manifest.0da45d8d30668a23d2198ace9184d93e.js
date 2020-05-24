d285b6530b785bcf3fa4794b9abe3744c96d27b1
Anil <anilmishra78@gmail.com>
2020-05-24 21:24:32 +0530

    Updates

---
+++ precache-manifest.0da45d8d30668a23d2198ace9184d93e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "676788efc3611e9c5a6c3da9014599f4",
+    "url": "/search-web/index.html"
+  },
+  {
+    "revision": "00cf5bf1271c51889fd6",
+    "url": "/search-web/static/css/main.b34e1095.chunk.css"
+  },
+  {
+    "revision": "12240f73676d8f2fbd85",
+    "url": "/search-web/static/js/2.a3c82928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/search-web/static/js/2.a3c82928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00cf5bf1271c51889fd6",
+    "url": "/search-web/static/js/main.7ab33b57.chunk.js"
+  },
+  {
+    "revision": "a078ad33969a6b94743b",
+    "url": "/search-web/static/js/runtime-main.152e4cf6.js"
+  }
+]);
